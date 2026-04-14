fab4f2f7b35f0b1f31c6455babac2da52457ab74
M. Tomanec <mtomanec7@gmail.com>
2026-04-14 12:31:42 +0200

    prekopane slozky a soubory s kodem

---
+++ firmware/dron/terminal-output/build/CMakeFiles/terminal-output.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/startup_stm32f405xx.s" "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/build/CMakeFiles/terminal-output.dir/startup_stm32f405xx.s.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F405xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/cmake/stm32cubemx/../../Core/Inc"
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/cmake/stm32cubemx/../../USB_DEVICE/App"
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/cmake/stm32cubemx/../../USB_DEVICE/Target"
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/cmake/stm32cubemx/../../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/cmake/stm32cubemx/../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/cmake/stm32cubemx/../../Middlewares/ST/STM32_USB_Device_Library/Core/Inc"
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/cmake/stm32cubemx/../../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc"
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/cmake/stm32cubemx/../../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/cmake/stm32cubemx/../../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/Core/Src/gpio.c" "CMakeFiles/terminal-output.dir/Core/Src/gpio.c.o" "gcc" "CMakeFiles/terminal-output.dir/Core/Src/gpio.c.o.d"
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/Core/Src/main.c" "CMakeFiles/terminal-output.dir/Core/Src/main.c.o" "gcc" "CMakeFiles/terminal-output.dir/Core/Src/main.c.o.d"
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/terminal-output.dir/Core/Src/stm32f4xx_hal_msp.c.o" "gcc" "CMakeFiles/terminal-output.dir/Core/Src/stm32f4xx_hal_msp.c.o.d"
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/Core/Src/stm32f4xx_it.c" "CMakeFiles/terminal-output.dir/Core/Src/stm32f4xx_it.c.o" "gcc" "CMakeFiles/terminal-output.dir/Core/Src/stm32f4xx_it.c.o.d"
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/Core/Src/syscalls.c" "CMakeFiles/terminal-output.dir/Core/Src/syscalls.c.o" "gcc" "CMakeFiles/terminal-output.dir/Core/Src/syscalls.c.o.d"
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/Core/Src/sysmem.c" "CMakeFiles/terminal-output.dir/Core/Src/sysmem.c.o" "gcc" "CMakeFiles/terminal-output.dir/Core/Src/sysmem.c.o.d"
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/USB_DEVICE/App/usb_device.c" "CMakeFiles/terminal-output.dir/USB_DEVICE/App/usb_device.c.o" "gcc" "CMakeFiles/terminal-output.dir/USB_DEVICE/App/usb_device.c.o.d"
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/USB_DEVICE/App/usbd_cdc_if.c" "CMakeFiles/terminal-output.dir/USB_DEVICE/App/usbd_cdc_if.c.o" "gcc" "CMakeFiles/terminal-output.dir/USB_DEVICE/App/usbd_cdc_if.c.o.d"
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/USB_DEVICE/App/usbd_desc.c" "CMakeFiles/terminal-output.dir/USB_DEVICE/App/usbd_desc.c.o" "gcc" "CMakeFiles/terminal-output.dir/USB_DEVICE/App/usbd_desc.c.o.d"
+  "/home/michal/projects/micro-controlers/firmware/dron/terminal-output/USB_DEVICE/Target/usbd_conf.c" "CMakeFiles/terminal-output.dir/USB_DEVICE/Target/usbd_conf.c.o" "gcc" "CMakeFiles/terminal-output.dir/USB_DEVICE/Target/usbd_conf.c.o.d"
+  "" "terminal-output" "gcc" "CMakeFiles/terminal-output.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ firmware/dron/terminal-output/build/CMakeFiles/terminal-output.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/terminal-output.dir/link.d"
+  "terminal-output.map"
+  "CMakeFiles/terminal-output.dir/Core/Src/gpio.c.o"
+  "CMakeFiles/terminal-output.dir/Core/Src/gpio.c.o.d"
+  "CMakeFiles/terminal-output.dir/Core/Src/main.c.o"
+  "CMakeFiles/terminal-output.dir/Core/Src/main.c.o.d"
+  "CMakeFiles/terminal-output.dir/Core/Src/stm32f4xx_hal_msp.c.o"
+  "CMakeFiles/terminal-output.dir/Core/Src/stm32f4xx_hal_msp.c.o.d"
+  "CMakeFiles/terminal-output.dir/Core/Src/stm32f4xx_it.c.o"
+  "CMakeFiles/terminal-output.dir/Core/Src/stm32f4xx_it.c.o.d"
+  "CMakeFiles/terminal-output.dir/Core/Src/syscalls.c.o"
+  "CMakeFiles/terminal-output.dir/Core/Src/syscalls.c.o.d"
+  "CMakeFiles/terminal-output.dir/Core/Src/sysmem.c.o"
+  "CMakeFiles/terminal-output.dir/Core/Src/sysmem.c.o.d"
+  "CMakeFiles/terminal-output.dir/USB_DEVICE/App/usb_device.c.o"
+  "CMakeFiles/terminal-output.dir/USB_DEVICE/App/usb_device.c.o.d"
+  "CMakeFiles/terminal-output.dir/USB_DEVICE/App/usbd_cdc_if.c.o"
+  "CMakeFiles/terminal-output.dir/USB_DEVICE/App/usbd_cdc_if.c.o.d"
+  "CMakeFiles/terminal-output.dir/USB_DEVICE/App/usbd_desc.c.o"
+  "CMakeFiles/terminal-output.dir/USB_DEVICE/App/usbd_desc.c.o.d"
+  "CMakeFiles/terminal-output.dir/USB_DEVICE/Target/usbd_conf.c.o"
+  "CMakeFiles/terminal-output.dir/USB_DEVICE/Target/usbd_conf.c.o.d"
+  "CMakeFiles/terminal-output.dir/startup_stm32f405xx.s.o"
+  "terminal-output"
+  "terminal-output.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/terminal-output.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
