51474f193a46126dbbddc7c6c7a84adcc1605118
Honza468 <janjpichl@gmail.com>
2026-04-17 16:29:59 +0200

    Implementovana zakladni verze Kalmanova filtru s MPU6500

--- FirmwareMotionController/FirmwareMotionController.ino
+++ FirmwareMotionController/FirmwareMotionController.ino
@@ -7,6 +7,10 @@ MPU6050 accelgyro;
 
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
+unsigned long lastTime;
+
+float angleX = 0;
+float angleY = 0;
 
 void setup() {
     Wire.begin(21, 22); // Inicializace I2C na pinech ESP32
@@ -18,6 +22,8 @@ void setup() {
     // Overeni spojeni
     Serial.println("Testovani pripojeni...");
     Serial.println(accelgyro.testConnection() ? "MPU6500 pripojen uspesne" : "MPU6500 pripojeni selhalo");
+
+    lastTime = micros();
 }
 
 void loop() {
@@ -33,14 +39,26 @@ void loop() {
     Serial.print(gx); Serial.print("\t");
     Serial.print(gy); Serial.print("\t");
     Serial.println(gz);*/
+    unsigned long currentTime = micros();
+    float dt = (currentTime - lastTime) / 1000000.0;
+    lastTime = currentTime;
 
     float accAngleX = atan2(ay, az) * RADTODEG;
     float accAngleY = atan2(-ax, sqrt(ay*ay + az*az)) * RADTODEG;
+
+    float gx_dps = gx / 131.0;
+    float gy_dps = gy / 131.0;
+
+    float alpha = 0.98;
+
+    angleX = alpha * (angleX + gx_dps * dt) + (1 - alpha) * accAngleX;
+    angleY = alpha * (angleY + gy_dps * dt) + (1 - alpha) * accAngleY;
+
     Serial.print("Osa X:\t");
-    Serial.println(accAngleX);
+    Serial.println(angleX);
     Serial.print("Osa Y:\t"); 
-    Serial.println(accAngleY);
+    Serial.println(angleY);
     Serial.println("---------------");
 
-    delay(100);
+    delay(5);
 }
