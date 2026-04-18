42e681d51704c443b9468ae7f4f38422f5ebd7f8
DominikStejskal <dominik.stejs@seznam.cz>
2026-04-18 14:37:13 +0200

    Pridan prepocet na LEDky

--- zkouska_obvodu_referencniho_napeti/zkouska_obvodu_referencniho_napeti.ino
+++ zkouska_obvodu_referencniho_napeti/zkouska_obvodu_referencniho_napeti.ino
@@ -3,6 +3,8 @@ const int pinRef = 35;    // Pin pro LM285 (2.5V)
 const float R1 = 100000.0;  //100K
 const float R2 = 100000.0;
 const float ratio = (R1 + R2) / R2;
+int led_pins[] = {4, 16, 2, 0, 15};
+int led_count = 5;
 
 void setup() {
   Serial.begin(115200);
@@ -21,11 +23,14 @@ void loop() {
   float vBattery = rawBat * ratio;
 
   // Mapování na procenta (zjednodušeně)
-  int percent = map(vBattery * 100, 320, 420, 0, 100); 
+  int percent = map(vBattery * 100, 320, 420, 0, 100);
+  int led_on = map(vBattery * 100, 320, 400, 1, 5);
   percent = constrain(percent, 0, 100);
+  led_on = constrain(led_on, 1, 5);
 
   Serial.print("Napeti: "); Serial.print(vBattery); Serial.println(" V");
   Serial.print("Nabiti: "); Serial.print(percent); Serial.println(" %");
+  Serial.print("Ledky: ");  Serial.print(led_on); Serial.println(" LED");
   
   delay(2000);
 }
