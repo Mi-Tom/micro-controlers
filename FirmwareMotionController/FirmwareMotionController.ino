2e7477aba7acfad7c7971e38fcb713c2a81adbc3
Honza468 <janjpichl@gmail.com>
2026-04-18 16:48:18 +0200

    Zkombinovany kody pro Akcelerometr a Gyroskop s kodem pro baterku

--- FirmwareMotionController/FirmwareMotionController.ino
+++ FirmwareMotionController/FirmwareMotionController.ino
@@ -4,8 +4,13 @@
 #define RADTODEG 57.2958
 #define MAXTILT 35.0
 #define MAXYAWANGLE 60.0
-#define POTPIN 34
+#define BUTTON_DELAY 50
 
+#define POTPIN 33       // Pin pro potenciomentr
+#define PIN_BAT 32      // Pin pro dělič baterie
+#define PIN_REF 35      // Pin pro LM285 (2.5V)
+
+//---- Gyroskop ----
 MPU6050 accelgyro;
 
 int16_t ax, ay, az;
@@ -17,6 +22,59 @@ float angleY = 0;
 float yaw_input_prev = 0.0;
 static float yaw_angle = 0;
 int p = 0;
+
+//---- Button Baterka ----
+const float R1 = 100000.0;  //100K
+const float R2 = 100000.0;
+const float VOLT_RATIO = (R1 + R2) / R2;
+
+int ledPins[] = { 4, 16, 2, 0, 15 };
+int ledCount = 5;
+
+struct Button {
+  int pin;
+  unsigned long lastPush;
+  bool lastState;
+  unsigned long offTime;
+};
+
+Button batBtn = { 27, 0, HIGH, 0 };
+
+// =====================TESTOVANI_NAPETI_BATERIE=====================
+
+void checkBattery() {
+  int refRaw = analogRead(PIN_REF);
+  int batRaw = analogRead(PIN_BAT);
+
+  float voltage = (float(batRaw) / refRaw) * 2.5 * VOLT_RATIO;
+  int percent = constrain((voltage - 3.2) * 100, 0, 100);
+  
+  int ledsToLight = map(percent, 0, 100, 0, ledCount);
+  ledsToLight = constrain(ledsToLight, 1, ledCount);
+
+  for (int i = 0; i < ledsToLight; i++) {
+    digitalWrite(ledPins[i], HIGH);
+  }
+
+  Serial.print("Napeti: ");
+  Serial.print(voltage);
+  Serial.println(" V");
+  Serial.print("Nabiti: ");
+  Serial.print(percent);
+  Serial.println(" %");
+  Serial.print("Ledky: ");
+  Serial.print(ledsToLight);
+  Serial.println(" LED");
+}
+
+
+void turnOffLeds() {
+  for (int i = 0; i < ledCount; i++) {
+    digitalWrite(ledPins[i], LOW);
+  }
+}
+
+// ==============================SETUP===============================
 
 void setup() {
     // Akcelerometr a Gyroskop
@@ -34,7 +92,15 @@ void setup() {
     // Potenciometr
     analogReadResolution(12); // 0–4095
     analogSetAttenuation(ADC_11db); // rozsah cca 0–3.3V
+
+    // Baterka
+    for (int i = 0; i < ledCount; i++) {
+        pinMode(ledPins[i], OUTPUT);
+    }
+    pinMode(batBtn.pin, INPUT_PULLUP);
 }
+
+// ===============================LOOP===============================
 
 void loop() {
     // Akcelerometr a Gyroskop
@@ -112,6 +178,25 @@ void loop() {
         Serial.println(throttle);
         Serial.println("---------------");
         p = 0;
+    }
+
+    // Baterka
+    unsigned long now = millis();
+    bool currentState = digitalRead(batBtn.pin);
+
+    if (batBtn.lastState == HIGH && currentState == LOW) {
+        if (now - batBtn.lastPush > BUTTON_DELAY) {
+        batBtn.lastPush = now;
+        checkBattery();
+        batBtn.offTime = now + 2000; // Po jak dlouhou dobu bude indikace baterie svítit
+        }
+    }
+    batBtn.lastState = currentState;
+
+    // Automatické vypnutí LED vázané na toto tlačítko
+    if (batBtn.offTime > 0 && now >= batBtn.offTime) {
+        turnOffLeds();
+        batBtn.offTime = 0;
     }
 
     delay(5);
